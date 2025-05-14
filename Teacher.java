2802a90ac8a79a56951a186e8f12852da0c2954b
MarinnaG <marinnajg@gmail.com>
2025-05-14 21:23:38 +0000

    getPassword method

--- Teacher.java
+++ Teacher.java
@@ -4,9 +4,13 @@ public class Teacher{
     public Teacher(){
         password = "000000";
     }
-    
+
     public Teacher(String p){
         password = p;
+    }
+    
+    public static String getPassword(){
+        return password;
     }
 
 }
