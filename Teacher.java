78b47645e31656b250b60711239bba63531ca384
MarinnaG <marinnajg@gmail.com>
2025-05-14 21:16:41 +0000

    Teacher class

---
+++ Teacher.java
@@ -0,0 +1,12 @@
+public class Teacher{
+    private String password;
+
+    public Teacher(){
+        password = "000000";
+    }
+    
+    public Teacher(String p){
+        password = p;
+    }
+
+}
