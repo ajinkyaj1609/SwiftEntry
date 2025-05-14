57ec028a859e5366aad64e0f26c0a05c1862cc17
papaya_1 <mayapkumanan@gmail.com>
2025-05-14 21:33:35 +0000

    students.java

---
+++ Students.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList; 
+
+public class Students{
+
+    ArrayList<Student> students;
+
+    public Students(ArrayList<Student> x){
+        students = x;
+    }
+
+    public void addStudent(int id){
+        Student x = new Student(id);
+        students.add(x);
+    }
+
+    public void removeStudents(int index){
+        students.remove(index);
+    }
+
+    public int findIndex(int id){
+        for (int i = 0; i < students.size(); i++){
+            if (students.get(i).getId() == id){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
