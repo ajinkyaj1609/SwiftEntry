f8df18ee56a5cce67f0e9cf76d568a444c465820
TheEmeraldPants <153216120+TheEmeraldPants@users.noreply.github.com>
2025-05-15 05:48:55 +0000

    EMAIL SUCCESSFULLY SENT

--- TLSEmail.java
+++ TLSEmail.java
@@ -1,11 +1,12 @@
 import java.util.Properties;
-
-import javax.mail.Authenticator;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
+import jakarta.mail.*;
+import jakarta.activation.*;
+import jakarta.mail.internet.*;
+import org.eclipse.angus.*;
 
 public class TLSEmail {
-
+	private static final String username = "swiftentryhost@gmail.com";
+	private static final String password = "tbct ttux fzta klms";
 	/**
 	   Outgoing Mail (SMTP) Server
 	   requires TLS or SSL: smtp.gmail.com (use authentication)
@@ -13,27 +14,46 @@ public class TLSEmail {
 	   Port for TLS/STARTTLS: 587
 	 */
 	public static void main(String[] args) {
-		final String fromEmail = "myemailid@gmail.com"; //requires valid gmail id
-		final String password = "mypassword"; // correct password for gmail id
-		final String toEmail = "myemail@yahoo.com"; // can be any email id 
 		
-		System.out.println("TLSEmail Start");
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
-		props.put("mail.smtp.port", "587"); //TLS Port
-		props.put("mail.smtp.auth", "true"); //enable authentication
-		props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
 		
-                //create Authenticator object to pass in Session.getInstance argument
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(fromEmail, password);
-			}
-		};
-		Session session = Session.getInstance(props, auth);
+		Properties prop = new Properties();
+		prop.put("mail.smtp.auth", true);
+		prop.put("mail.smtp.starttls.enable", "true");
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.tls.trust", "smtp.gmail.com");
+		prop.put("mail.stmp.tls.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 		
-		EmailUtil.sendEmail(session, toEmail,"TLSEmail Testing Subject", "TLSEmail Testing Body");
+		Session session = Session.getInstance(prop, new Authenticator() {
+		@Override
+		protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password);
+		}
+		});
 		
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(
+			Message.RecipientType.TO, InternetAddress.parse("em98723122@gmail.com"));
+			message.setSubject("Mail Subject");
+
+			String msg = "This is my first email using JavaMailer";
+
+			MimeBodyPart mimeBodyPart = new MimeBodyPart();
+			mimeBodyPart.setContent(msg, "text/html; charset=utf-8");
+
+			Multipart multipart = new MimeMultipart();
+			multipart.addBodyPart(mimeBodyPart);
+
+			message.setContent(multipart);
+			
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
 	}
 }
