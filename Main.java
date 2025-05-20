39a53de03ff59c43aade8c7d172e679f74e31f80
papaya_1 <mayapkumanan@gmail.com>
2025-05-20 21:00:41 +0000

    semi-working scan method

--- Main.java
+++ Main.java
@@ -1,14 +1,43 @@
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main
 {
+
+        private static Scanner input = new Scanner(System.in);
+
         public static void main(String[] args) {
 
+                //adding student to test scan method 
+                Students list = new Students(new ArrayList<Student>());
+                list.addStudent(123);
+
+                //the code that takes in the input, checks which student 
+                String sT = "13:55:00"; //this time is just to check, set it to whatever time the teacher wants later
+                String eT = "14:18:09";
+
+                DateTimeFormatter theFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+                LocalTime startTime = LocalTime.parse(sT, theFormat);
+                LocalTime endTime = LocalTime.parse(eT, theFormat);
+                LocalTime currTime = LocalTime.now();
+
+                //while loop
+                while(currTime.isAfter(startTime)){
+                        System.out.println("Type in student's id for scanning");
+                        int id = input.nextInt();
+                        int index = list.findIndex(id); 
+                        list.accessScanner(index);
+                }
+
+                //file reading check
                 String data="data.txt";
                 File file = new File(data);
                 //read from data.txt
@@ -49,21 +78,24 @@ public class Main
                 catch (IOException e){
                         System.out.println("Error reading file: " + e.getMessage());
                 }
-                /*  
-                public static int firstScan(){ // checks if studet has already check in for the day. Used to distinguish attendence check ins from brief during class check outs.
-                        for(int i = 0; i<testingStudents.length; i++){
-                                if(student.getName() != testingStudent[i]){
-                                        return 1; // if 1 is return if is the first check in of the day, and therefore the attendence check in
-                                }
-                                else{
-                                        System.out.println("The student has already checked in for attendence")
-                                        return 0
-                                }
-                        }
-                }
-                public static int scanNumber(){
-                        if(student.get)
-                }
-                */
+                
         }
 }
+
+/*  
+public static int firstScan(){ // checks if studet has already check in for the day. Used to distinguish attendence check ins from brief during class check outs.
+        for(int i = 0; i<testingStudents.length; i++){
+                if(student.getName() != testingStudent[i]){
+                        return 1; // if 1 is return if is the first check in of the day, and therefore the attendence check in
+                }
+                else{
+                        System.out.println("The student has already checked in for attendence")
+                        return 0
+                }
+        }
+}
+                
+public static int scanNumber(){
+        if(student.get)
+}
+*/
