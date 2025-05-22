1c1b219e00e2b3c736d7537e9dfd5335a3a480a5
papaya_1 <mayapkumanan@gmail.com>
2025-05-22 17:42:55 +0000

    commenting out code not needed

--- Main.java
+++ Main.java
@@ -8,18 +8,15 @@ public class Main {
     public static ArrayList<Student> students = new ArrayList<>();
 
     public static void main(String[] args){
-        //moved everything to ScannerTesting.java
-        String[] names = {"Alice", "Bob", "Charlie"};
-        int[] ids = {101, 102, 103};
-        for (int i = 0; i < ids.length; i++) {
-            Student s = new Student(names[i], Integer.toString(ids[i]));
-            students.add(s);
-        }
-
-        // Print the students
-        for (Student s : students) {
-            System.out.println(s);
-        }
+        // String[] names = {"Alice", "Bob", "Charlie"};
+        // int[] ids = {101, 102, 103};
+        // for (int i = 0; i < ids.length; i++) {
+        //     Student s = new Student(names[i], Integer.toString(ids[i]));
+        //     students.add(s);
+        // }
+        // for (Student s : students) {
+        //     System.out.println(s);
+        // }
         Teacher prof = new Teacher();
         prof.startDay();
        
